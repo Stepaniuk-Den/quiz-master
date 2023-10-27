000e5b4230171ecfad18e03eda6e6a708ef66a44
GKastet <127600582+GKastet@users.noreply.github.com>
2023-10-27 20:26:45 +0200

    favorite, own quiz page filtration

--- src/modules/ownQuizPage/components/QuizFilterTools/QuizFilterTools.jsx
+++ src/modules/ownQuizPage/components/QuizFilterTools/QuizFilterTools.jsx
@@ -3,12 +3,12 @@ import QuizeSearch from "../QuizSearch/QuizSearch";
 import { FormFiltersWrapper } from "./QuizFilterToolsStyled";
 //import QuizeSearch from "../QuizSearch/QuizSearch";
 
-const QuizeFilterTools = () => {
+const QuizeFilterTools = ({filteredQuizeCards}) => {
   // const location = useLocation()
   // const locationPath = location.pathname === '/discover'? true : false;
   return (
     <FormFiltersWrapper>
-      <QuizeSearch />
+      <QuizeSearch filteredQuizeCards={filteredQuizeCards}/>
     </FormFiltersWrapper>
   );
 };
--- src/pages/OwnQuizPage/OwnQuizPage.jsx
+++ src/pages/OwnQuizPage/OwnQuizPage.jsx
@@ -4,17 +4,29 @@ import BtnLoadMore from "../../shared/Buttons/BtnLoadMore/BtnLoadMore";
 import PageTopBar from "../../shared/PageTopBar/PageTopBar";
 import QuizesList from "../../shared/QuizesList/QuizesList";
 import { PageWrapper, SectionWrapper } from "./OwnQuizePageStyled";
+import {quizNames} from '../../shared/QuizesList/quizNames'
+import { useState } from "react";
 
 const OwnQuizPage = () => {
   //const location = useLocation();
-  // console.log("locationDiscovery", location);
+  
+  const newQuizCategoriesNames = quizNames.map((name) => name.quizCategory);
+  // console.log(newQuizCategoriesNames);
+  const [quizCategoriesNameArr, setQuizCategoriesNameArr] = useState(newQuizCategoriesNames);
+  //const location = useLocation();
+  // console.log("locationFavorite", location);
+  const filteredQuizeCards = (filteredNames) => {    
+    setQuizCategoriesNameArr(filteredNames);
+  };
+
+  // console.log("quizArr", quizArr);
 
   return (
     <PageWrapper>
       <SectionWrapper>
         <PageTopBar titlePage="My quiz" />
-        <QuizeFilterTools />
-        <QuizesList />
+        <QuizeFilterTools filteredQuizeCards={filteredQuizeCards}/>
+        <QuizesList quizCategoriesNameArr={quizCategoriesNameArr}/>
         <BtnLoadMore />
       </SectionWrapper>
     </PageWrapper>
