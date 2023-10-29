a0eafde08c39255001ba62bdc41cd259e3003f25
Sankerp <sankerp78@gmail.com>
2023-10-29 21:03:15 +0200

    SettingsPage-update

---
+++ src/modules/settingsPage/settingsUserAvatar.jsx
@@ -0,0 +1,10 @@
+// import { settingsUserAvatar } from "./settingsUserAvatarStyled";
+// import {infoUser} from "./info/infoUser";
+
+// const settingsAvatar = () => {
+//     return (
+//         <settingsUserAvatar src={infoUser.avatar} alt="user avatar" />
+//     )
+// }
+  
+// export default settingsAvatar();
