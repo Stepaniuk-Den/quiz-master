17a4214233472087c022f7aea8174a0896abe669
Alina Demchenko <demalina88@gmail.com>
2023-10-29 14:18:56 -0700

    avatar

--- src/modules/mainPage/components/Review/Review.jsx
+++ src/modules/mainPage/components/Review/Review.jsx
@@ -1,11 +1,12 @@
 import { StyledReview } from "./Review.styled";
 import CardTitle from "../../../../shared/CardTitle/CardTitle";
 import Paragraph from "../../../../shared/Paragraph/Paragraph";
+import Avatar from "../../../../shared/Avatar/Avatar";
 
 function Review() {
   return (
     <StyledReview>
-      <img src="https://c.files.bbci.co.uk/9270/production/_126588473_boris_1221_getty.jpg" alt="avatar"  width="44" />
+      <Avatar src="https://c.files.bbci.co.uk/9270/production/_126588473_boris_1221_getty.jpg" alt="avatar" width="44"/>
       <CardTitle>Name</CardTitle>
       <Paragraph>
         Excellent platform, I myself passed more than one quiz, I recommended it
