f2793817d502b1992f47aa3597a71fbf7c270c5e
Sankerp <sankerp78@gmail.com>
2023-10-29 10:37:13 +0200

    Settings page

--- src/pages/SettingsPage/SettingsPage.jsx
+++ src/pages/SettingsPage/SettingsPage.jsx
@@ -1,8 +1,49 @@
 
+import UserStats from "../../modules/homepage/components/UserStats"
+import {
+  // BoxUserStats,
+  PageWrapper,
+  SectionWrapper,
+  TitleSettings,
+  TitleWrapper,
+} from "./SettingsPageStyled";
+
+// Перша літера на аватар
+// function firstLetterToAvatar(str) {
+//   if (str.length > 0) {
+//     const firstLetter = str.charAt(0).toUpperCase();
+//     console.log(firstLetter);
+//   }
+// }
+// const userName = "";
+// firstLetterToAvatar(userName);
+
 const SettingsPage = () => {
+
   return (
-    <div>SettingsPage</div>
+    <>
+    <PageWrapper>
+        <SectionWrapper>
+          firstLetter;
+        <TitleWrapper>
+          <button
+            style={{
+              color: "#000000",
+              fontFamily: "Roboto",
+              fontSize: 16,
+              width: 42,
+              marginBottom: 16,
+            }}
+          >
+            Back
+          </button>
+          <TitleSettings>Settings</TitleSettings>
+        </TitleWrapper>
+        <UserStats />
+      </SectionWrapper>
+    </PageWrapper>
+      </>
   )
 }
 
-export default SettingsPage
+export default SettingsPage;
