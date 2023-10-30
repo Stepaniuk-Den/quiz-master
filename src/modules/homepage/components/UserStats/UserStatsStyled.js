93889d327ad21a4633635a00165a7b653d9133a6
Inna Zlotska <innaz002001@gmail.com>
2023-10-31 00:09:33 +0200

    bg image, аdaptive layout

--- src/modules/homepage/components/UserStats/UserStatsStyled.js
+++ src/modules/homepage/components/UserStats/UserStatsStyled.js
@@ -5,17 +5,24 @@ export const UserCard = styled.div`
   height: 420px;
   border-radius: 20px;
   background: var(--blue-background);
-  padding: 32px 32px 0 32px;
+  padding: 32px;
   text-align: center;
   margin: auto;
   margin-bottom:80px;
   @media (min-width: 768px) {
     max-width: 100%;
     margin-bottom:24px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    height: 100%;
+    padding: 32px;
   }
   @media (min-width: 1440px) {
+    display: block;
     width: 292px;
     margin: 0;
+    height: 420px;
 margin-bottom:124px;
   }
 `;
@@ -50,4 +57,35 @@ export const Line = styled.div`
 border-top: 1px solid rgba(244, 244, 244, 0.20);;
 width: 100%; 
 margin: 16px 0;
+
+  @media (min-width: 768px) {
+   display: none;
+  }
+  @media (min-width: 1440px){
+    display: block;
+   }
 `;
+
+export const PassedBox = styled.div`
+display:block;
+ @media (min-width: 768px) {
+   display: flex;
+  }
+  @media (min-width: 1440px){
+    display: block;
+   }
+`
+
+export const LineHeight = styled.div`
+display: none;
+    @media (min-width: 768px) {
+      display: block;
+   border-right: 1px solid rgba(244, 244, 244, 0.20);
+  height: 58px;
+  margin: 0 16px;
+  }
+
+   @media (min-width: 1440px){
+    display: none;
+   }
+`
--- src/pages/LastQuizesPage/LastQuizesPageStyled.js
+++ src/pages/LastQuizesPage/LastQuizesPageStyled.js
@@ -1,4 +1,10 @@
 import styled from "styled-components";
 
 export const ContainerLastPassed = styled.div`
+display: flex;
+    flex-direction: column;
+    align-items: center;
+ @media (min-width: 768px) {
+    display: block;
+  }
 `
