3e879b88e452bd8cd05d6a3aee845ae2008f89a5
Inna Zlotska <innaz002001@gmail.com>
2023-10-30 20:56:28 +0200

    fix home-page

--- src/pages/LastQuizesPage/LastQuizesPage.jsx
+++ src/pages/LastQuizesPage/LastQuizesPage.jsx
@@ -1,13 +1,15 @@
+import { useSelector } from "react-redux";
 import BtnLoadMore from "../../shared/components/Buttons/BtnLoadMore/BtnLoadMore"
 import PageTopBar from "../../shared/components/PageTopBar/PageTopBar"
 import QuizesList from "../../shared/components/QuizesList/QuizesList"
-import { quizNames } from "../../shared/components/QuizesList/quizNames"
+import { selectPassed } from "../../redux/selectors";
 
 const LastQuizesPage = () => {
+    const passedQuizzes = useSelector(selectPassed);
   return (
     <div>
       <PageTopBar titlePage="Last passed quizzes" />
-      <QuizesList quizCategoriesNameArr={quizNames} />
+      <QuizesList quizzesArr={passedQuizzes} />
       <BtnLoadMore/>
     </div>
   )
