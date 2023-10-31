d358d31678ea07fc58e10b4a13caab982f611d0c
Alina Demchenko <demalina88@gmail.com>
2023-10-30 22:05:38 -0700

    Avatar

--- src/redux/user/userSelectors.js
+++ src/redux/user/userSelectors.js
@@ -1,3 +1,4 @@
 export const selectUser = state => state.user.user;
 export const selectToken = state => state.user.token;
 export const selectIsAuth = state => state.user.isAuth;
+export const selectUserAvatar = state => state.user.avatar;
