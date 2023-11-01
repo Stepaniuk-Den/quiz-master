31df7307890a1daa2c74b4866eb15cb8d880ea79
pttkachuk <petrotkachuk6@gmail.com>
2023-11-01 10:51:57 +0100

    random page

---
+++ src/pages/RandomQuizPage/RandomQuizPageStyled.js
@@ -0,0 +1,65 @@
+import styled from "styled-components";
+
+export const RandomPageWrapper = styled.div`
+  //outline: 1px solid green;
+  padding: 80px 0 0;
+  //display: flex;
+  //flex-direction: column;
+  //align-items: center;
+`;
+
+export const RandomSectionWrapper = styled.section`
+  outline: 1px solid red;
+  width: 100%;
+  max-width: 1240px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+// export const RandomTitleWrapper = styled.div`
+//   margin: 0;
+//   padding: 0;
+// `;
+
+export const RandomPageDesc = styled.div`
+  width: 100%;
+  margin-top: 20px;
+  margin-bottom: 40px;
+
+  p {
+    display: inline-block;
+    width: 100%;
+    font-size: 14px;
+  }
+
+  @media only screen and (min-width: 375px) {
+    p {
+      width: 335px;
+    }
+  }
+
+  @media only screen and (min-width: 768px) {
+    margin-top: 24px;
+      margin-bottom: 48px;
+    p {
+      width: 584px;
+       font-size: 16px;
+    }
+  }
+
+  @media only screen and (min-width: 1440px) {
+    p {
+      width: 660px;
+    }
+  }
+`;
+
+
+
+
+
+
+
+
+
