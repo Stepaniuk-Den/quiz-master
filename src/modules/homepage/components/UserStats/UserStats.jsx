cb80c52881701fd70966cd057c5f164c19df71d9
Inna Zlotska <innaz002001@gmail.com>
2023-11-02 21:35:21 +0200

    home-page

--- src/modules/homepage/components/UserStats/UserStats.jsx
+++ src/modules/homepage/components/UserStats/UserStats.jsx
@@ -1,32 +1,43 @@
-
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import Avatar from "../../../../shared/components/Avatar/Avatar";
 import { InfoText, Line, LineHeight, PassedBox, TitleText, UserAvatar, UserCard, UserName } from "./UserStatsStyled";
 import {infoUser} from "./info/infoUser";
 import { selectUserAvatar } from "../../../../redux/user/userSelectors";
+import { getTotalPassedThunk } from "../../../../redux/quiz/quizThunks";
+import { useEffect, useState } from "react";
+import { currentUserThunk } from "../../../../redux/user/userThunks";
 
 const UserStats = () => {
-const userAvatar = useSelector(selectUserAvatar);
+  const dispatch = useDispatch();
+  const [fetchedData, setFetchedData] = useState(null);
+
+  useEffect(() => {
+    dispatch(currentUserThunk()).then((data) => {
+      setFetchedData(data.payload);
+    });
+  }, [dispatch]);
+
+const averageSuccess = fetchedData ? Math.round(fetchedData.average) : '';
+
   return (
     <UserCard>
-      <Avatar size="large" src={infoUser.avatar} alt="User avatar" width="100px"/>
-      <UserName>{infoUser.userName}</UserName>
+      <Avatar size="large" src={fetchedData ? fetchedData.avatar : ''} alt="User avatar" width="100px" />
+      <UserName>{fetchedData ? fetchedData.name : ''}</UserName>
       <PassedBox>
         <div>
           <TitleText>Passed quizzes</TitleText>
-          <InfoText>{infoUser.passedQuizzes}</InfoText>
+          <InfoText>{fetchedData ? fetchedData.passedQuizzes.length : ''}</InfoText>
         </div>
           
         <Line />
         <LineHeight/>
         <div>
           <TitleText>Average success</TitleText>
-        <InfoText>{infoUser.averageSuccess}%</InfoText>
+          <InfoText>{fetchedData ? `${averageSuccess}%` : ''}</InfoText>
         </div>
-          
-        </PassedBox>
+      </PassedBox>
     </UserCard>
-  )
+  );
 }
 
 export default UserStats;
