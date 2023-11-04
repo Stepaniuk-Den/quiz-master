1e6e1c1f8d6af7cb7c19d73cb9a4576bbef022fb
Inna Zlotska <innaz002001@gmail.com>
2023-11-04 23:34:57 +0200

    quiz page

--- src/modules/quizPage/QuizQuestion/QuizQuestionStyled.js
+++ src/modules/quizPage/QuizQuestion/QuizQuestionStyled.js
@@ -15,3 +15,20 @@ export const NextButton = styled.button`
 padding: 16px 62px;
 border-radius: 30px;
 background: #205BF1;`
+
+export const StyledButton = styled.button`
+  padding: 14px 171px 14px 18px;
+  border-radius: 30px;
+  border: 1px solid rgba(244, 244, 244, 0.60);
+  background-color: transparent;
+  cursor: pointer;
+background-color: ${props =>
+  props.isCorrect === 'correct' ? 'green' :
+  props.isCorrect === 'incorrect' ? 'red' :
+  'transparent'};
+  color: ${props =>
+    props.isCorrect === 'correct' ? 'white' :
+    props.isCorrect === 'incorrect' ? 'white' :
+    'black'};
+
+`;
