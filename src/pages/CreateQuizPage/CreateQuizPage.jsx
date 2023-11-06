ab857e10c275c05cc8bbd300cd46bb237368938a
Stepaniuk-Den <stepaniuk.den@gmail.com>
2023-11-06 13:34:02 +0200

    create components

--- src/pages/CreateQuizPage/CreateQuizPage.jsx
+++ src/pages/CreateQuizPage/CreateQuizPage.jsx
@@ -1,21 +1,14 @@
+import QuestionsList from "../../modules/createQuizPage/components/QuestionsList/QuestionsList.jsx";
 import PageTopBar from "../../shared/components/PageTopBar/PageTopBar.jsx";
-import SelectCustom from "../../shared/components/Select/Select.jsx";
-import { PageWrapper, QuestionList, QuestionsWrapper, SectionWrapper } from "./CreateQuizPage.styled.js";
+// import SelectCustom from "../../shared/components/Select/Select.jsx";
+import { PageWrapper, SectionWrapper } from "./CreateQuizPage.styled.js";
 
 const CreateQuizPage = () => {
   return (
     <PageWrapper>
       <PageTopBar titlePage="Create quize" />
       <SectionWrapper>
-        <QuestionsWrapper>
-          <p>Questions</p>
-          <QuestionList>
-            <li>quiz</li>
-          </QuestionList>
-          <SelectCustom name="create" id="" selectTitle={'selectTitle'}>
-            create
-          </SelectCustom>
-        </QuestionsWrapper>
+        <QuestionsList/>
         <div>
           <input type="text" placeholder="Quiz theme" />
           <div>formQuiz</div>
