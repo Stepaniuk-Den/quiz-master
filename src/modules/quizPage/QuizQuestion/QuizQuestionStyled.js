16ebaffa2fae9c759838ab672a7debb5922d1606
Vlad Melnik <vmelniki2828@gmail.com>
2023-11-06 03:03:48 +0200

    styled and add back button to quiz card

--- src/modules/quizPage/QuizQuestion/QuizQuestionStyled.js
+++ src/modules/quizPage/QuizQuestion/QuizQuestionStyled.js
@@ -1,38 +1,96 @@
 import styled from "styled-components";
-import isPropValid from '@emotion/is-prop-valid';
+import isPropValid from "@emotion/is-prop-valid";
 
 // export const = styled
 export const TitleQuestion = styled.h2`
-color: #F4F4F4;
-font-size: 24px;
+  color: var(--text-color-100);
+  font-size: 24px;
 
-font-weight: 700;
-line-height: 32px;
-letter-spacing: -0.24px;
-margin-bottom:40px;`
-
-export const NextButton = styled.button`
-padding: 16px 62px;
-border-radius: 30px;
-background: #205BF1;`
-
-
-
-export const StyledButton = styled.button.withConfig({
-   shouldForwardProp: (prop) => isPropValid(prop),
-})`
-  padding: 14px 171px 14px 18px;
-  border-radius: 30px;
-  border: 1px solid rgba(244, 244, 244, 0.60);
-  background-color: transparent;
-  cursor: pointer;
-  background-color: ${props =>
-    props.isCorrect === 'correct' ? 'green' :
-    props.isCorrect === 'incorrect' ? 'red' :
-    'transparent'};
-  color: ${props =>
-    props.isCorrect === 'correct' ? 'white' :
-    props.isCorrect === 'incorrect' ? 'white' :
-    'black'};
+  font-weight: 700;
+  line-height: 32px;
+  letter-spacing: -0.24px;
+  margin-bottom: 40px;
 `;
 
+export const NextButton = styled.button`
+  padding: 16px 62px;
+  border-radius: 30px;
+  background: var(--blue-background);
+
+  font-style: normal;
+  font-weight: 700;
+  font-size: 16px;
+  line-height: 16px;
+  letter-spacing: -0.01em;
+
+  color: var(--text-color-100);
+`;
+
+export const AnswersContainer = styled.ul`
+  display: flex;
+  flex-wrap: wrap;
+  gap: 16px;
+  margin-top: 16px;
+`;
+
+export const ButtonText = styled.p`
+  color: var(--text-color-100);
+  font-size: 16px;
+  line-height: 16px;
+  letter-spacing: -0.01em;
+`;
+
+export const AnswerLabels = styled.span`
+  font-style: normal;
+  font-weight: 700;
+`;
+
+export const BackButton = styled.button`
+  background: none;
+  border: none;
+  color: white;
+  cursor: pointer;
+  margin-left: 14px;
+  font-weight: 700;
+  font-size: 16px;
+  line-height: 16px;
+  letter-spacing: -0.01em;
+  color: var(--text-color-100);
+  border-bottom: 2px solid var(--text-color-100);
+`;
+
+export const DownContainer = styled.div`
+  position: relative;
+  margin-top: 40px;
+`;
+
+export const AnswersCounter = styled.p`
+  position: absolute;
+  bottom: 0;
+  right: 0;
+  margin: 0; /* Убираем внешние отступы */
+`;
+
+export const StyledButton = styled.button.withConfig({
+  shouldForwardProp: (prop) => isPropValid(prop),
+})`
+  width: 222px;
+  text-align: left;
+  padding: 14px 0 14px 18px;
+  border-radius: 30px;
+  border: 1px solid rgba(244, 244, 244, 0.6);
+  background-color: transparent;
+  cursor: pointer;
+  background-color: ${(props) =>
+    props.isCorrect === "correct"
+      ? "green"
+      : props.isCorrect === "incorrect"
+      ? "red"
+      : "transparent"};
+  color: ${(props) =>
+    props.isCorrect === "correct"
+      ? "white"
+      : props.isCorrect === "incorrect"
+      ? "white"
+      : "black"};
+`;
--- src/modules/quizPage/Time/Time.jsx
+++ src/modules/quizPage/Time/Time.jsx
@@ -1,31 +1,38 @@
-import React from 'react';
-import Countdown from 'react-countdown';
+import React from "react";
+import { StyledCountdown, TimeText } from "./Time.styled";
+import Countdown from "react-countdown";
 
 const Time = ({ question, currentQuestion, isAnswerSelected }) => {
-    const formatTime = (timeInSeconds) => {
-        const minutes = Math.floor(timeInSeconds / 60);
-        const seconds = timeInSeconds % 60;
-        return `${String(minutes).padStart(2, '0')}:${String(seconds).padStart(2, '0')}`;
-    };
+  const formatTime = (timeInSeconds) => {
+    const minutes = Math.floor(timeInSeconds / 60);
+    const seconds = timeInSeconds % 60;
+    return `${String(minutes).padStart(2, "0")}:${String(seconds).padStart(
+      2,
+      "0"
+    )}`;
+  };
 
-    return (
-        <div>
-            <p>Time: 
-            <Countdown
-                date={Date.now() + question.time * 1000}
-                key={currentQuestion}
-                renderer={({ minutes, seconds, completed }) => {
-                    if (completed || isAnswerSelected) {
-                        // Якщо таймер завершено або ви обрали відповідь, зупиняємо лічильник
-                        return <span>{formatTime(0)}</span>;
-                    } else {
-                        return <span>{formatTime(minutes * 60 + seconds)}</span>;
-                    }
-                }}
-            /></p>
-        </div>
-    );
+  return (
+    <>
+      <TimeText>
+        Time:
+        <StyledCountdown>
+          <Countdown
+            date={Date.now() + question.time * 1000}
+            key={currentQuestion}
+            renderer={({ minutes, seconds, completed }) => {
+              if (completed || isAnswerSelected) {
+                // Якщо таймер завершено або ви обрали відповідь, зупиняємо лічильник
+                return <span>{formatTime(0)}</span>;
+              } else {
+                return <span>{formatTime(minutes * 60 + seconds)}</span>;
+              }
+            }}
+          />
+        </StyledCountdown>
+      </TimeText>
+    </>
+  );
 };
 
 export default Time;
-
