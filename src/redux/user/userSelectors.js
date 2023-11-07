57d505f88ada4153e995ede13235af3693df9e33
Alina Demchenko <demalina88@gmail.com>
2023-11-06 18:59:48 -0800

    fixed merged

--- src/redux/user/userSelectors.js
+++ src/redux/user/userSelectors.js
@@ -2,3 +2,4 @@ export const selectUser = state => state.user.user;
 export const selectToken = state => state.user.token;
 export const selectIsAuth = state => Boolean(state.user.token);
 export const selectUserAvatar = state => state.user.avatar;
+export const selectIsRefreshing = state => state.user.isRefreshing;
