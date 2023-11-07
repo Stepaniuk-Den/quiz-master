eee00200d1f160065431570c0a001ebfbd6b6b73
Leo007ua <leo.oliinyk@ukr.net>
2023-11-07 19:11:27 +0200

    first comit

--- src/modules/createQuizPage/components/QuestionsList/QuestionsList.jsx
+++ src/modules/createQuizPage/components/QuestionsList/QuestionsList.jsx
@@ -1,19 +1,54 @@
-import React from 'react'
-import { QuestionList, QuestionsWrapper } from './QuestionsList.styled'
-import SelectCustom from '../../../../shared/components/Select/Select'
+import React from "react";
+import { OptionSt, QuestionList, QuestionsWrapper, SelectStyledQuiz } from "./QuestionsList.styled";
 
-const QuestionsList = () => {
+import { useState } from "react";
+import { v4 as uuidv4 } from "uuid"; // для генерації унікальних ідентифікаторів
+import { FiTrash2 } from 'react-icons/fi';
+
+function QuestionsList() {
+  const [selectedOption, setSelectedOption] = useState("Create");
+  const [selectedOptions, setSelectedOptions] = useState([]);
+
+  const handleOptionChange = (event) => {
+    const newOption = event.target.value;
+
+    if (newOption) {
+      setSelectedOptions([...selectedOptions, { id: uuidv4(), value: newOption }]);
+      setSelectedOption("Create"); // встановлення "Create" після додавання нового пункту
+    }
+  };
+
+  const handleDeleteOption = (id) => {
+    const updatedOptions = selectedOptions.filter((option) => option.id !== id);
+    setSelectedOptions(updatedOptions);
+  };
+
   return (
     <QuestionsWrapper>
-          <p>Questions</p>
-          <QuestionList>
-            <li>quiz</li>
+      <p>Questions</p>
+      <ul>
+        {selectedOptions.map((option, index) => (
+          <QuestionList key={option.id}>
+            {index + 1}. {option.value}{" "}
+            <span
+              role="button"
+              onClick={() => handleDeleteOption(option.id)}
+              style={{ cursor: "pointer" }}
+            >
+            <FiTrash2/>
+            </span>
           </QuestionList>
-          <SelectCustom name="create" id="" selectTitle={'selectTitle'}>
-            create
-          </SelectCustom>
-        </QuestionsWrapper>
-  )
+        ))}
+      </ul>
+     
+      <SelectStyledQuiz value={selectedOption} onChange={handleOptionChange}>
+        <option value="Create">Create</option>
+        <option value="Quiz">Quiz</option>
+        <option value="True or false">True or false</option>
+      </SelectStyledQuiz>
+    </QuestionsWrapper>
+  );
 }
 
-export default QuestionsList
+export default QuestionsList;
+
--- src/modules/createQuizPage/components/QuestionsList/QuestionsList.styled.js
+++ src/modules/createQuizPage/components/QuestionsList/QuestionsList.styled.js
@@ -1,6 +1,5 @@
 import styled from "styled-components";
 
-
 export const QuestionsWrapper = styled.div`
   display: flex;
   flex-direction: column;
@@ -11,6 +10,7 @@ export const QuestionsWrapper = styled.div`
   padding: 32px;
 
   & p {
+    padding-bottom: 32px;
     font-size: 24px;
     font-style: normal;
     font-weight: 700;
@@ -19,5 +19,38 @@ export const QuestionsWrapper = styled.div`
 `;
 
 export const QuestionList = styled.ul`
-  margin-top: 32px;
+  justify-content: space-between; 
+  padding-bottom: 12px;
+  display: flex;
+  align-items: center;
+  margin: 8px 0;
+  list-style-type: none;
+  border-bottom: 1px solid #ccc; 
 `;
+
+export const SelectStyledQuiz = styled.select`
+
+  margin-top: 32px;
+  width: 100%;
+  max-width: 335px;
+  height: 48px;
+  padding: 15px 18px;
+  border-radius: 30px;
+
+  border: 1px solid var(--border-color-30);
+  background-color: transparent;
+  color: #F4F4F4;
+  color: var(--text-color-100);
+  font-size: 16px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 1.29; /* 128.571% */
+  letter-spacing: -0.14px;
+  /* Стилізація опцій (колір тексту та фону) */
+  option {
+    /* color: red; */
+    background: blue; /* Колір фону опцій */
+  }
+`;
+export const OptionSt = styled.option`
+background:red;`
