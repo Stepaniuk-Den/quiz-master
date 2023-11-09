8d66bb68221e95c68c4d4d121858d5eb6798deff
Leo007ua <leo.oliinyk@ukr.net>
2023-11-09 13:13:58 +0200

    fix section

--- src/modules/createQuizPage/components/QuestionsList/QuestionsList.jsx
+++ src/modules/createQuizPage/components/QuestionsList/QuestionsList.jsx
@@ -1,46 +1,28 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useState } from "react";
+
 import { v4 as uuidv4 } from "uuid";
 import { FiTrash2 } from "react-icons/fi";
-
+import { FiChevronDown, FiChevronUp } from "react-icons/fi";
+import { HiArrowLongRight } from "react-icons/hi2";
 import {
   QuestionsWrapper,
   QuestionList,
-  Up,
-  Down,
-  DropdownList,
-  DropdownContainer,
-  DropdownButton,
-  DropdownItem,
+  SelectStyledQuiz,
+  DropdownList, 
 } from "./QuestionsList.styled";
 
+
 function QuestionsList() {
-  const maxVisibleOptions = 8;
-  const [selectedOption, setSelectedOption] = useState("Create");
+  const maxOptions = 10;
   const [selectedOptions, setSelectedOptions] = useState([]);
-  const [isDropdownTimeOpen, setDropdownTimeOpen] = useState(false);
-  const [inputValue, setInputValue] = useState("");
-  const [quizItems, setQuizItems] = useState([]);
-  const [selectedQuizItem, setSelectedQuizItem] = useState(null);
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
 
-  const dropdownRef = useRef(null);
-  const inputRef = useRef(null);
-
-  const toggleDropdown = () => {
-    setDropdownTimeOpen(!isDropdownTimeOpen);
-  };
-
-  const handleOptionChange = (event) => {
-    const newOption = event.target.value;
-
-    if (newOption === "Create") {
-      setSelectedOption("Create");
-      setInputValue("");
-      setQuizItems([]);
-    } else {
-      setSelectedOption(newOption);
-
-      const optionToAdd = { id: uuidv4(), value: newOption };
-      setSelectedOptions([...selectedOptions, optionToAdd]);
+  const handleAdd = (event) => {
+    if (selectedOptions.length < maxOptions) {
+      setSelectedOptions([
+        ...selectedOptions,
+        { id: uuidv4(), value: event.target.textContent },
+      ]);
     }
   };
 
@@ -49,40 +31,11 @@ function QuestionsList() {
     setSelectedOptions(updatedOptions);
   };
 
-  const handleAddQuizItem = () => {
-    if (inputValue.trim() !== "") {
-      setQuizItems([...quizItems, { id: uuidv4(), value: inputValue }]);
-      setInputValue("");
-    }
-  };
-
-  const handleDeleteQuizItem = (id) => {
-    const updatedQuizItems = quizItems.filter((item) => item.id !== id);
-    setQuizItems(updatedQuizItems);
-  };
-
-  useEffect(() => {
-    const handleDocumentTimeClick = (event) => {
-      if (
-        isDropdownTimeOpen &&
-        dropdownRef.current &&
-        !dropdownRef.current.contains(event.target)
-      ) {
-        setDropdownTimeOpen(false);
-      }
-    };
-    document.addEventListener("click", handleDocumentTimeClick);
-
-    return () => {
-      document.removeEventListener("click", handleDocumentTimeClick);
-    };
-  }, [isDropdownTimeOpen]);
-
   return (
     <QuestionsWrapper>
       <p>Questions</p>
-      <ul>
-        {selectedOptions.slice(0, maxVisibleOptions).map((option, index) => (
+      <ul style={{ maxHeight: isDropdownOpen ? "200px" : "auto", overflowY: "auto" }}>
+        {selectedOptions.map((option, index) => (
           <QuestionList key={option.id}>
             {index + 1}. {option.value}{" "}
             <span
@@ -94,74 +47,34 @@ function QuestionsList() {
             </span>
           </QuestionList>
         ))}
-        {selectedOptions.length > maxVisibleOptions && (
-          <QuestionList>
-            {selectedOptions.length - maxVisibleOptions} more...
-          </QuestionList>
-        )}
       </ul>
-
-      <DropdownContainer ref={dropdownRef}>
-        <DropdownButton onClick={toggleDropdown} lab="true">
-          {selectedOption === "Create" ? "Create" : selectedOption}
-          {isDropdownTimeOpen ? <Up /> : <Down />}
-        </DropdownButton>
-        <DropdownList open={isDropdownTimeOpen}>
-          <DropdownItem value="Quiz" onClick={() => setSelectedOption("Quiz")}>Quiz</DropdownItem>
-          <DropdownItem value="True or false" onClick={() => setSelectedOption("True or false")}>True or false</DropdownItem>
+      <SelectStyledQuiz
+        onClick={() => {
+          setIsDropdownOpen(!isDropdownOpen);
+        }}
+      >
+        Create
+        {isDropdownOpen ? (
+          <FiChevronDown size={14} />
+        ) : (
+          <FiChevronUp size={14} />
+        )}
+      </SelectStyledQuiz>
+      {isDropdownOpen && (
+        <DropdownList>
+          <ul>
+            <li>
+              <button onClick={handleAdd}>
+                Quiz <HiArrowLongRight size={24} />
+              </button>
+            </li>
+            <li>
+              <button onClick={handleAdd}>
+                True or false <HiArrowLongRight size={24} />
+              </button>
+            </li>
+          </ul>
         </DropdownList>
-      </DropdownContainer>
-
-      {selectedOption === "Quiz" && (
-        <div>
-          <input
-            ref={inputRef}
-            type="text"
-            placeholder="Enter a quiz item"
-            value={inputValue}
-            onChange={(e) => setInputValue(e.target.value)}
-          />
-          <button onClick={handleAddQuizItem}>Add Quiz Item</button>
-          <ol>
-            {quizItems.map((item, index) => (
-              <li key={item.id}>
-                {index + 1}. {item.value}{" "}
-                <span
-                  role="button"
-                  onClick={() => handleDeleteQuizItem(item.id)}
-                  style={{ cursor: "pointer" }}
-                >
-                  <FiTrash2 />
-                </span>
-              </li>
-            ))}
-          </ol>
-        </div>
-      )}
-
-      {selectedOption === "True or false" && (
-        <div>
-          <ol>
-            {quizItems.map((item, index) => (
-              <li key={item.id}>
-                {index + 1}. {item.value}{" "}
-                <span
-                  role="button"
-                  onClick={() => {
-                    if (selectedQuizItem === item.id) {
-                      setSelectedQuizItem(null);
-                    } else {
-                      setSelectedQuizItem(item.id);
-                    }
-                  }}
-                  style={{ cursor: "pointer" }}
-                >
-                  {selectedQuizItem === item.id ? "Selected" : "Select"}
-                </span>
-              </li>
-            ))}
-          </ol>
-        </div>
       )}
     </QuestionsWrapper>
   );
